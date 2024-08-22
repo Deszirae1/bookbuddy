03fe8d8c9f220723f7579b71ba8dd5c9fb357616
Deszirae1 <166359401+Deszirae1@users.noreply.github.com>
2024-08-22 07:50:18 -0700

    Add files via upload
    api

---
+++ api/users.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const userRouter = express.Router();
+const { getUserById, getUsers } = require("../db/users");
+
+userRouter.get("/", async (req, res) => {
+  try {
+    const results = await getUsers();
+    res.send(results);
+  } catch (err) {
+    res.send({ err, message: "something went wrong" });
+  }
+});
+
+// {baseUrl}/users/id
+userRouter.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await getUserById(id);
+    res.send(result);
+  } catch (err) {
+    res.send({ err, message: "something went wrong" });
+  }
+});
+
+// {baseUrl}/users/me
+userRouter.get("/me", (req, res) => {
+  res.send("here is your account info");
+});
+
+// POST request to {baseUrl}/api/users/register
+userRouter.post("/register", (req, res) => {
+  console.log("REQUEST BODY", req.body);
+  res.send("User registered");
+});
+
+userRouter.post("/login", (req, res) => {
+  console.log("REQUEST BODY", req.body);
+  res.send("You logged in successfully!");
+});
+
+module.exports = userRouter; 
