9ddcc2c7ed05ac3766c0efd1535f7048a2de9f08
Deszirae1 <166359401+Deszirae1@users.noreply.github.com>
2024-10-06 21:02:10 -0700

    final changes

--- db/books.js
+++ db/books.js
@@ -9,7 +9,7 @@ const getBooks = async () => {
     }
     return rows;
   } catch (err) {
-    console.log(err);
+    res.send(err);
   }
 };
 
@@ -40,8 +40,9 @@ const createBook = async ({
       title,
       author,
       description,
-      coverImage,
-      available,
+      coverImage ||
+        "https://images.pexels.com/photos/7034646/pexels-photo-7034646.jpeg",
+      available || true,
     ]);
     return book;
   } catch (err) {
@@ -49,4 +50,29 @@ const createBook = async ({
   }
 };
 
-module.exports = { createBook, getBooks, getBook }; 
+const deleteBook = async (id) => {
+  try {
+    const SQL = `DELETE FROM books WHERE id=$1 RETURNING *`;
+    const {
+      rows: [result],
+    } = await client.query(SQL, [id]);
+
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const updateBook = async (id, available) => {
+  try {
+    const SQL = `UPDATE books SET available=$1 WHERE id=$2 RETURNING *`;
+    const {
+      rows: [book],
+    } = await client.query(SQL, [available, id]);
+    return book;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { createBook, getBooks, getBook, deleteBook, updateBook };
--- db/users.js
+++ db/users.js
@@ -1,17 +1,26 @@
 const client = require("./client");
-const bcrypt = require('bcrypt');
-const SALT_COUNT = 10; 
+const bcrypt = require("bcrypt");
+const SALT_COUNT = 10;
 
-const createUser = async ({ firstname, lastname, email, password }) => {
+const createUser = async ({
+  firstname = "ASDFASDF",
+  lastname = "ASDASDF",
+  email,
+  password,
+}) => {
   try {
     const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
     console.log(hashedPassword);
     const SQL = `INSERT INTO users(firstname, lastname, email, password) VALUES($1, $2, $3, $4) ON CONFLICT(email) DO NOTHING RETURNING id, firstname, lastname, email`;
-    
     const {
       rows: [user],
-    } = await client.query(SQL, [firstname, lastname, email, hashedPassword]);
-    console.log("rows", user);
+    } = await client.query(SQL, [
+      firstname || "firstname",
+      lastname || "lastname",
+      email,
+      hashedPassword,
+    ]);
+
     return user;
   } catch (err) {
     console.log(err);
@@ -31,12 +40,30 @@ const getUserByEmail = async (email) => {
   }
 };
 
+const getUser = async ({ email, password }) => {
+  console.log(email);
+  try {
+    const existingUser = await getUserByEmail(email);
+    if (!existingUser) return;
+    const hashedPassword = existingUser.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (!passwordsMatch) return;
+    console.log(existingUser);
+    delete existingUser.password;
+    console.log("existing user", existingUser);
+    return existingUser;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const getUserById = async (id) => {
   try {
     const SQL = `SELECT * FROM users WHERE id=$1`;
     const {
       rows: [user],
     } = await client.query(SQL, [id]);
+    delete user.password;
     return user;
   } catch (err) {
     console.log(err);
@@ -53,4 +80,4 @@ const getUsers = async () => {
   }
 };
 
-module.exports = { createUser, getUserByEmail, getUserById, getUsers };
+module.exports = { createUser, getUserByEmail, getUserById, getUsers, getUser };
